9d50f2a0ab01f203d269eaecf2fe8ea65210144b
sandeep <sandeepblh@gmail.com>
2019-06-02 00:24:24 +1000

    kafka example

---
+++ src/main/java/com/demo/kafka/springbootwithkafka/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.demo.kafka.springbootwithkafka.controller;
+
+import com.demo.kafka.springbootwithkafka.engine.Producer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
---
+++ src/main/java/com/demo/kafka/springbootwithkafka/models/User.java
@@ -0,0 +1,10 @@
+package com.demo.kafka.springbootwithkafka.models;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private int age;
+}
